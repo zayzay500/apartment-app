4fa5e395dc12a8a401d9419cf7b90b99cb9b8477
zayzay500 <lindsay.pierce@yale.edu>
2020-06-17 13:46:53 -0400

    initial setup

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,30 @@
 class ApartmentsController < ApplicationController
   # add controller methods
+  get '/apartments' do
+  
+  end
+  
+  get '/apartments/new' do
+  
+  end
+  
+  get 'apartments/:id' do
+  
+  end
+  
+  post 'apartments/' do
+  
+  end
+  
+  patch 'apartments/:id' do
+  
+  end
+  
+  delete 'apartments/:id' do
+  
+  end
+  
+  def get_apartment
+    Apartment.find(params[:id])
+  end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,31 @@
 class TenantsController < ApplicationController
   # add controller methods
+  
+  get '/tenants' do 
+  
+  end
+  
+  get '/tenants/new' do
+  
+  end
+  
+  get '/tenants/:id' do 
+  
+  end
+  
+  post '/tenants' do 
+  
+  end
+  
+  patch '/tenants/:id' do  
+  
+  end
+  
+  delete '/tenants/:id' do 
+  
+  end
+
+  def get_tennant
+    Tenant.find(params[:id])
+  end
 end
